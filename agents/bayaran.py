513a6f01d2c2bf1099db64831c15f8adbca93a2e
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-03-22 10:18:46 +0700

    Penyesuaian atribut agen bayaran

---
+++ agents/bayaran.py
@@ -0,0 +1,28 @@
+from agents.base import RandomWalker
+from agents.pemilih import Pemilih
+
+
+class Bayaran(RandomWalker):
+    """
+    Agen bayaran yang bergerak secara acak memberikan uang ke pemilih.
+    """
+
+    def __init__(self, unique_id, pos, model, moore, uang=None):
+        super().__init__(unique_id, pos, model, moore=moore)
+        self.uang = uang
+
+    def step(self):
+        self.random_move()
+
+        this_cell = self.model.grid.get_cell_list_contents([self.pos])
+        pemilih = [obj for obj in this_cell if isinstance(obj, Pemilih)]
+
+        if len(pemilih) > 0:
+            pengaruhi_pemilih = self.random.choice(pemilih)
+            pengaruhi_pemilih.pemberian = self.model.pengeluaran_uang
+
+            self.uang -= self.model.pengeluaran_uang
+
+        if self.uang <= 0:
+            self.model.grid._remove_agent(self.pos, self)
+            self.model.schedule.remove(self)
